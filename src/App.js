82f60edacd65a7d3e1feb3b6980fd217c9cbc5ed
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-15 14:45:33 -0300

    Estrutura do projeto

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@
 //importar a biblioteca
 import React from 'react';
  const App = () => {
-     return <h1>Anne Karoline</h1>
+     return (
+         <div className="container">
+             <div className="valid-keys">
+                <div className="matched">Ann</div>
+                <div className="remainder">e</div>
+             </div>
+             
+             <div className="typed-keys">asfemerasf</div>
+
+             <div className="completed-works">
+                 <ol>
+                     <li>cidade</li>
+                     <li>carro</li>
+                     <li>profissional</li>
+                 </ol>
+             </div>
+         </div>
+     )
 
  }
 
