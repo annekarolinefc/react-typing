058886456547291d214543b075fcf0c032540895
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-09-15 14:13:18 -0300

    Iniciando o Projeto do zero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+//Compontente da Aplicação
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//importar a biblioteca
+import React from 'react';
+ const App = () => {
+     return <h1>Anne Karoline</h1>
 
-export default App;
+ }
+
+ //exportar o componente
+ export default App;
